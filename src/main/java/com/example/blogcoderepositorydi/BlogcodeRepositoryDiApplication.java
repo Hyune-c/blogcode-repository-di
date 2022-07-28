8d92bc4b7a13948f9f504d2dd30090dd3c083425
hyune <choi8608@gmail.com>
2022-07-29 02:06:57 +0900

    Initial commit

---
+++ src/main/java/com/example/blogcoderepositorydi/BlogcodeRepositoryDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogcoderepositorydi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcodeRepositoryDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogcodeRepositoryDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogcoderepositorydi/BlogcodeRepositoryDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogcoderepositorydi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogcodeRepositoryDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
